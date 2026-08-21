f9937fec406e6c425836939b3bb6ebee01ff6a26
hsnalmajed <almajedhsn@gmail.com>
2026-08-21 13:42:15 +0300

    Add files via upload

--- src/app/[locale]/maps/page.tsx
+++ src/app/[locale]/maps/page.tsx
@@ -3,7 +3,7 @@ import type { Locale } from "@/lib/types";
 import { findCountry } from "@/lib/countries";
 import { COUNTRY_GUIDES } from "@/lib/countryGuides";
 import { COUNTRY_CITIES } from "@/lib/cities";
-import { fetchWikiSummaries } from "@/lib/wikipedia";
+import { fetchCountryPhotos } from "@/lib/countryPhotos";
 import MapsCountryList, { type MapCountry } from "@/components/MapsCountryList";
 
 // Photos are fetched live from Wikipedia, same as the rest of the site.
@@ -18,19 +18,17 @@ export default async function MapsPage({ params }: PageProps<"/[locale]/maps">) 
   // is now a list of city maps, so a country with none would lead to an
   // empty page.
   const codes = Object.keys(COUNTRY_GUIDES).filter((c) => (COUNTRY_CITIES[c]?.length ?? 0) > 0);
-  const titles = codes.map((c) => COUNTRY_GUIDES[c].attractions[0].wikiTitle);
-  const summaries = await fetchWikiSummaries(titles);
+  const photos = await fetchCountryPhotos(codes);
 
   const countries: MapCountry[] = codes
     .map((code): MapCountry | null => {
       const country = findCountry(code);
       if (!country) return null;
-      const summary = summaries.get(COUNTRY_GUIDES[code].attractions[0].wikiTitle);
       return {
         code,
         nameAr: country.nameAr,
         nameEn: country.nameEn,
-        photo: summary?.thumbnail,
+        photo: photos.get(code),
         cityCount: COUNTRY_CITIES[code].length,
       };
     })
